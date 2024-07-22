359fe387e4e8f524c027e81d2c5249a34c52923c
uwen_godwin <uwengodwin001@gmail.com>
2024-07-22 16:20:01 +0100

    pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,16 +1,17 @@
-#!/usr/bin/env python3
-"""
-Simple pagination
-"""
-
+from typing import List, Tuple
 import csv
-from typing import List
+import math
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Returns a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for those
+    particular pagination parameters.
+    """
     start_index = (page - 1) * page_size
     end_index = page * page_size
-    return (start_index, end_index)
+    return start_index, end_index
 
 
 class Server:
@@ -31,7 +32,15 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Get a page of the dataset"""
+        """
+        Takes two integer arguments page with default value 1
+        and page_size with default value 10.
+        Uses index_range to find the correct indexes to paginate
+        the dataset correctly and return
+        the appropriate page of the dataset (i.e., the correct list of rows).
+        If the input arguments are out of range for the dataset,
+        an empty list should be returned.
+        """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
