b2b288fc98bb6f113ac178f3ba310519716f0ca2
uwen_godwin <uwengodwin001@gmail.com>
2024-07-22 16:27:18 +0100

    pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,18 +1,16 @@
-#!/usr/bin/env python3
-"""
-Hypermedia pagination
-"""
-
+from typing import List, Tuple, Dict
 import csv
 import math
-from typing import List, Dict, Any
-
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Returns a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for those
+    particular pagination parameters.
+    """
     start_index = (page - 1) * page_size
     end_index = page * page_size
-    return (start_index, end_index)
-
+    return start_index, end_index
 
 class Server:
     """Server class to paginate a database of popular baby names."""
@@ -32,7 +30,12 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Get a page of the dataset"""
+        """
+        Takes two integer arguments page with default value 1 and page_size with default value 10.
+        Uses index_range to find the correct indexes to paginate the dataset correctly and return
+        the appropriate page of the dataset (i.e., the correct list of rows).
+        If the input arguments are out of range for the dataset, an empty list should be returned.
+        """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
@@ -44,11 +47,22 @@ class Server:
 
         return dataset[start_index:end_index]
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
-        """Get a hypermedia page of the dataset"""
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, any]:
+        """
+        Takes two integer arguments page with default value 1 and page_size with default value 10.
+        Returns a dictionary with key-value pairs as described:
+        - page_size: the length of the returned dataset page
+        - page: the current page number
+        - data: the dataset page (the same as get_page)
+        - next_page: number of the next page, None if no next page
+        - prev_page: number of the previous page, None if no previous page
+        - total_pages: the total number of pages in the dataset
+        """
         data = self.get_page(page, page_size)
-        total_pages = math.ceil(len(self.dataset()) / page_size)
-        return {
+        total_items = len(self.dataset())
+        total_pages = math.ceil(total_items / page_size)
+
+        hypermedia = {
             'page_size': len(data),
             'page': page,
             'data': data,
@@ -56,3 +70,5 @@ class Server:
             'prev_page': page - 1 if page > 1 else None,
             'total_pages': total_pages
         }
+
+        return hypermedia
