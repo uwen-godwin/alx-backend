c939edcffa7fbf4e608c5b409a2c66a8c8d0bd52
uwen_godwin <uwengodwin001@gmail.com>
2024-07-24 10:36:25 +0100

    pagination

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -4,73 +4,6 @@ Deletion-resilient hypermedia pagination
 """
 
 import csv
-from typing import List, Dict
-
-
-class Server:
-    """Server class to paginate a database of popular baby names."""
-    DATA_FILE = "Popular_Baby_Names.csv"
-
-    def __init__(self):
-        self.__dataset = None
-        self.__indexed_dataset = None
-
-    def dataset(self) -> List[List]:
-        """Cached dataset."""
-        if self.__dataset is None:
-            with open(self.DATA_FILE) as f:
-                reader = csv.reader(f)
-                dataset = [row for row in reader]
-            self.__dataset = dataset[1:]
-        return self.__dataset
-
-    def indexed_dataset(self) -> Dict[int, List]:
-        """Dataset indexed by sorting position, starting at 0."""
-        if self.__indexed_dataset is None:
-            dataset = self.dataset()
-            self.__indexed_dataset = {i:dataset[i] for i in range(len(dataset))}
-        return self.__indexed_dataset
-
-    def get_hyper_index(self, index: int = 0, page_size: int = 10) -> Dict:
-        """Provide pagination that is resilient to row deletions."""
-        assert isinstance(index, int) and index >= 0
-        assert isinstance(page_size, int) and page_size > 0
-        
-        dataset = self.indexed_dataset()
-        total_items = len(dataset)
-        
-        if index >= total_items:
-            raise AssertionError("Index out of range")
-
-        data = []
-        current_index = index
-        next_index = index
-
-        while len(data) < page_size and next_index < total_items:
-            item = dataset.get(next_index, None)
-            if item:
-                data.append(item)
-                current_index = next_index
-            next_index += 1
-
-        next_index = current_index + 1
-
-        while next_index < total_items and next_index not in dataset:
-            next_index += 1
-
-        if next_index >= total_items:
-            next_index = None
-
-        return {
-            'index': index,
-            'next_index': next_index,
-            'page_size': page_size,
-            'data': data#!/usr/bin/env python3
-"""
-Deletion-resilient hypermedia pagination
-"""
-
-import csv
 from typing import List, Dict, Any
 
 
@@ -143,5 +76,4 @@ class Server:
             'next_index': next_index,
             'page_size': page_size,
             'data': data
-        }
         }
