a5bd5845c7fecd464ff9814e4bc12c7e18871b0e
uwen_godwin <uwengodwin001@gmail.com>
2024-07-25 09:56:57 +0100

    Caching

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -10,17 +10,28 @@ class MRUCache(BaseCaching):
     def __init__(self):
         """ Initialize """
         super().__init__()
+        self.usage_order = []
 
     def put(self, key, item):
         """ Add an item in the cache """
         if key is None or item is None:
             return
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            last_key = next(reversed(self.cache_data))
+
+        if key in self.cache_data:
+            self.usage_order.remove(key)
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            last_key = self.usage_order.pop()
             print("DISCARD:", last_key)
-            self.cache_data.pop(last_key)
+            del self.cache_data[last_key]
+
         self.cache_data[key] = item
+        self.usage_order.append(key)
 
     def get(self, key):
         """ Get an item by key """
-        return self.cache_data.get(key, None)
+        if key is None or key not in self.cache_data:
+            return None
+
+        self.usage_order.remove(key)
+        self.usage_order.append(key)
+        return self.cache_data[key]
