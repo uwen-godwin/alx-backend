bb205cf6d123d7d585ba1f15a97b7eeca9df4cd3
uwen_godwin <uwengodwin001@gmail.com>
2024-07-28 09:30:39 +0100

    pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -34,13 +34,13 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
-        Takes two integer arguments page with default
-        value 1 and page_size with default value 10.
-        Uses index_range to find the correct indexes to
-        paginate the dataset correctly and return
+        Takes two integer arguments page with default value 1
+        and page_size with default value 10.
+        Uses index_range to find the correct indexes to paginate
+        the dataset correctly and return
         the appropriate page of the dataset (i.e., the correct list of rows).
-        If the input arguments are out of range for the
-        dataset, an empty list should be returned.
+        If the input arguments are out of range for the dataset,
+        an empty list should be returned.
         """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
@@ -55,8 +55,8 @@ class Server:
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
         """
-        Takes two integer arguments page with default
-        value 1 and page_size with default value 10.
+        Takes two integer arguments page with default value 1
+        and page_size with default value 10.
         Returns a dictionary with key-value pairs as described:
         - page_size: the length of the returned dataset page
         - page: the current page number
@@ -69,12 +69,15 @@ class Server:
         total_items = len(self.dataset())
         total_pages = math.ceil(total_items / page_size)
 
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
         hypermedia = {
             'page_size': len(data),
             'page': page,
             'data': data,
-            'next_page': page + 1 if page < total_pages else None,
-            'prev_page': page - 1 if page > 1 else None,
+            'next_page': next_page,
+            'prev_page': prev_page,
             'total_pages': total_pages
         }
 
