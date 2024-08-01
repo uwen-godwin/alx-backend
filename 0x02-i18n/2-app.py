629a9742dfa957dcac7ed26e3a28e72080453f9b
uwen_godwin <uwengodwin001@gmail.com>
2024-08-01 20:52:05 +0100

    i18n

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -7,24 +7,29 @@ from flask_babel import Babel
 
 app = Flask(__name__)
 
+
 class Config:
     """Config class"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app.config.from_object(Config)
 babel = Babel(app)
+
 
 @babel.localeselector
 def get_locale():
     """Get locale from request"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route('/')
 def index():
     """Get locale from request"""
     return render_template('2-index.html')
+
 
 if __name__ == '__main__':
     app.run()
