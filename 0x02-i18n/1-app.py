5709a3a81930d6c928ca96b80958f50001be7b4b
uwen_godwin <uwengodwin001@gmail.com>
2024-08-01 20:50:54 +0100

    i18n

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -7,19 +7,23 @@ from flask_babel import Babel
 
 app = Flask(__name__)
 
+
 class Config:
     """Config class"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app.config.from_object(Config)
 babel = Babel(app)
+
 
 @app.route('/')
 def index():
     """Basic Flask app with Babel"""
     return render_template('1-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
